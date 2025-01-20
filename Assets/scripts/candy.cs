a9660397c2be852e2e0529517aa56c6c41d90bfc
lavi <lavinaorab@gmail.com>
2025-01-20 13:40:06 +0200

    candy script

---
+++ Assets/scripts/candy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class candy : MonoBehaviour
+{
+
+    public candyType candyType;
+
+    public int xIndex;
+    public int yIndex;
+
+    public bool isMatched;
+    private Vector2 current;
+    private Vector2 targetPause;
+
+    public bool isMoving;
+
+    public candy(int _x, int _y)
+    {
+        xIndex = _x;
+        yIndex = _y;
+    }
+
+}
+
+public enum candyType
+{
+    Red,
+    Blue,
+    Green,
+    Purple,
+    White
+}
