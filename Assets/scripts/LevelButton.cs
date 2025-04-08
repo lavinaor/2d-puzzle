55a20ced6ee726f587588121b36b979de5663133
lavi <lavinaorab@gmail.com>
2025-04-08 16:45:04 +0300

    stars save start bugs

---
+++ Assets/scripts/LevelButton.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : SceneChanger
+{
+    [SerializeField]
+    private List<Image> stars; // רשימת תמונות של כוכבים
+
+    [SerializeField]
+    private int level; // מספר שלב
+
+    private void Start()
+    {
+        int starsNum = SaveManager.Instance.GetStarsForLevel(level);
+        Debug.Log("כוכבים לשלב " + level + ": " + starsNum);
+        UpdateStars(starsNum);
+    }
+
+    void UpdateStars(int starsNum)
+    {
+        // מכבה את כל הכוכבים
+        foreach (var star in stars)
+        {
+            star.gameObject.SetActive(false);
+        }
+
+        // מדליק את הכמות המתאימה
+        for (int i = 0; i < starsNum && i < stars.Count; i++)
+        {
+            stars[i].gameObject.SetActive(true);
+        }
+    }
+}
