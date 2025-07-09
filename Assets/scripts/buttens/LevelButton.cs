925195394c494f5c89e2e78b5f036750a40b1d03
lavi <lavinaorab@gmail.com>
2025-07-09 13:56:37 +0300

    lock add back

--- Assets/scripts/buttens/LevelButton.cs
+++ Assets/scripts/buttens/LevelButton.cs
@@ -76,11 +76,6 @@ public class LevelButton : MonoBehaviour
             if (button != null) button.interactable = false;
         }
 
-        isUnlocked = true;
-        if (lockedVisual != null) lockedVisual.SetActive(false);
-        if (unlockedVisual != null) unlockedVisual.SetActive(true);
-        if (button != null) button.interactable = true;
-
         UpdateGrayVisuals();
     }
 
