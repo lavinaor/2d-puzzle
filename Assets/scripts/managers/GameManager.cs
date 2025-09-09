f983f3606bc5a6489d77bb9dda8daff9f8ebdbe3
lavi <lavinaorab@gmail.com>
2025-09-09 16:59:14 +0300

    star enimashen

--- Assets/scripts/managers/GameManager.cs
+++ Assets/scripts/managers/GameManager.cs
@@ -1,73 +1,52 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
-    //הופך אותו לסטטי
     public static GameManager Instance;
 
-    //הכמות שצריך כדי לנצח את השלב
-    public int goal;
-    // כמות הצעדים שיש לך 
-    public int moves;
-    //שומר את הניקוד
-    public int points;
+    [Header("Gameplay Settings")]
+    public int goal;          // כמות נקודות לניצחון
+    public int moves;         // צעדים שנותרו
+    public int points;        // ניקוד נוכחי
+    public bool isGameEnded;  // האם המשחק הסתיים
 
-    //שומר האם המשחק נגמר
-    public bool isGameEnded;
+    [SerializeField] private List<Goal> goals = new List<Goal>();
 
-    // רשימה של מטרות לכל סוג ממתק
-    [SerializeField]
-    private List<Goal> goals = new List<Goal>();
-
-    //השומר את הכיתובים על המסך
+    [Header("UI")]
     public TMP_Text pointText;
     public TMP_Text movesText;
     public TMP_Text goalText;
 
-    // נוסיף משתנה לשמירת ה-Slider
     public Slider scoreSlider;
 
-    // הוספת רשימה לכוכבים
-    public List<Image> stars;
+    [Header("Stars")]
+    public List<Image> stars; // כוכבים במהלך המשחק
+    public List<Image> starsEndScreen; // כוכבים במסך הסיום
+    [SerializeField] private Color emptyStarColor = new Color(0.2f, 0.2f, 0.2f, 1f); // צבע כוכב ריק
+    [SerializeField] private Color filledStarColor = Color.white;                     // צבע כוכב מלא
     private int starCount = 0;
 
-    // קובץ הסאונד להשמעה
-    [SerializeField]
-    private AudioClip starSound;
+    [Header("Audio & VFX")]
+    [SerializeField] private AudioClip starSound;
+    [SerializeField] private AudioClip winSound;
+    [SerializeField] private AudioClip loseSound;
+    [SerializeField] private GameObject vfx;
+    [SerializeField] private GameObject winVFX;
+    [SerializeField] private GameObject loseVFX;
 
-    // אפקט בפריפב
-    [SerializeField]
-    private GameObject vfx;
-
-    [Header("End screen")]
-    //מסך ניצחון
+    [Header("End Screen")]
     public GameObject[] victoryPanel = new GameObject[0];
-    //מסך הפסד
     public GameObject losePanel;
-
-    public List<Image> starsEndScree;
 
     private string areaName;
     private int levelNum;
-
-    // קובץ הסאונד להשמעה
-    [SerializeField]
-    private AudioClip winSound;
-    [SerializeField]
-    private AudioClip loseSound;
-
-    // אפקטים לניצחון והפסד
-    [SerializeField]
-    private GameObject winVFX;
-    [SerializeField]
-    private GameObject loseVFX;
 
     private void Awake()
     {
@@ -76,6 +55,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        // חישוב מטרה כוללת לפי כל היעדים
         if (goals.Count != 0)
         {
             goal = 0;
@@ -84,17 +64,31 @@ public class GameManager : MonoBehaviour
                 goal += g.goalPerType;
             }
         }
-        // עדכון ה-Slider שיתחיל מ-0 עם הגבלת ה-Max
+
+        // הגדרת הסליידר
         scoreSlider.maxValue = goal;
         scoreSlider.value = 0;
+
+        foreach (var star in stars)
+            star.color = emptyStarColor;
+
+        foreach (var star in starsEndScreen)
+            star.color = emptyStarColor;
+
+
         ParseSceneName();
+    }
+
+    private void Update()
+    {
+        pointText.text = "Points: " + points;
+        movesText.text = moves.ToString();
+        goalText.text = "Goals: " + goal;
     }
 
     void ParseSceneName()
     {
         string sceneName = SceneManager.GetActiveScene().name;
-
-        // פיצול לפי מקף
         string[] parts = sceneName.Split('-');
 
         if (parts.Length == 2)
@@ -108,23 +102,14 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    // עיבוד תור
+    public void ProcessTurn(List<candy> candiesToProcess, bool _subtractMoves)
     {
-        pointText.text = "Points: " + points.ToString();
-        movesText.text = moves.ToString();
-        goalText.text = "goals: " + goal.ToString();
-    }
-
-    public void ProcessTurn(List<candy> candiesToProcess, bool _subtracMoves)
-    {
-
         if (goals.Count != 0)
         {
-            // עבור כל ממתק ברשימה
             foreach (candy c in candiesToProcess)
             {
-                // הפחת ממטרה תואמת לסוג הממתק
+                // יעד לפי סוג ממתק
                 Goal specificGoal = goals.Find(g => g.type == c.candyType && g.goalPerType > 0);
                 if (specificGoal != null)
                 {
@@ -132,12 +117,12 @@ public class GameManager : MonoBehaviour
                     points++;
                 }
 
-                // הפחת גם ממטרה רנדומלית אם קיימת
+                // יעד רנדומלי
                 Goal randomGoal = goals.Find(g => g.type == CandyType.random && g.goalPerType > 0);
                 if (randomGoal != null)
                 {
                     randomGoal.goalPerType--;
-                    points++; // רק אם לא ספרנו כבר בנקודה קודם
+                    points++;
                 }
             }
         }
@@ -146,105 +131,182 @@ public class GameManager : MonoBehaviour
             points += candiesToProcess.Count;
         }
 
-        // קריאה לעדכון הכוכבים
         UpdateStars();
 
-        // מחשב את הערך של הסליידר בטווח 0-1
+        // עדכון הסליידר
         if (goal > 0)
         {
-            scoreSlider.value = (float)points/* / goal*/;
+            scoreSlider.value = points;
         }
 
-        //מוריד מהלכים אם צריך
-        if (_subtracMoves)
-        {
+        if (_subtractMoves)
             moves--;
-        }
 
-
-
-
-
-        // סיום המשחק
+        // תנאי סיום
         if (points >= goal || (moves == 0 && starCount > 0))
         {
-            isGameEnded = true;
-
-            // אפקט של ניצחון
-            Instantiate(winVFX, victoryPanel[0].transform.position, Quaternion.identity);
-
-            //הפעל מצב ניצחון
-            PopUpManger.Instance.ChangeUIState(4);
-            foreach (var panel in victoryPanel)
-            {
-                panel.SetActive(true);
-            }
-
-            //סאונד של ניצחון
-            SoundFXManager.Instance.PlaySoundFXClip(winSound, transform, 1f, false);
+            EndGame(true);
             return;
         }
 
         if (moves == 0 && starCount <= 0)
         {
-            isGameEnded = true;
-
-            // אפקט של הפסד
-            Instantiate(loseVFX, losePanel.transform.position, Quaternion.identity);
-
-            //הפעל מצב הפסד
-            PopUpManger.Instance.ChangeUIState(4);
-            losePanel.SetActive(true);
-
-            //סאונד של הפסד
-            SoundFXManager.Instance.PlaySoundFXClip(loseSound, transform, 1f, false);
+            EndGame(false);
             return;
         }
     }
 
+    // סיום משחק
+    void EndGame(bool isWin)
+    {
+        isGameEnded = true;
+
+        if (isWin)
+        {
+            Instantiate(winVFX, victoryPanel[0].transform.position, Quaternion.identity);
+            PopUpManger.Instance.ChangeUIState(4);
+
+            foreach (var panel in victoryPanel)
+                panel.SetActive(true);
+
+            SoundFXManager.Instance.PlaySoundFXClip(winSound, transform, 1f, false);
+
+            // אנימציה של כוכבי מסך סיום
+            AnimateEndScreenStars(starsEndScreen, starCount);
+        }
+        else
+        {
+            Instantiate(loseVFX, losePanel.transform.position, Quaternion.identity);
+            PopUpManger.Instance.ChangeUIState(4);
+            losePanel.SetActive(true);
+            SoundFXManager.Instance.PlaySoundFXClip(loseSound, transform, 1f, false);
+        }
+    }
+
+    // עדכון כוכבים במשחק
     void UpdateStars()
     {
         float progress = (float)points / goal;
 
         if (progress >= 0.5f && starCount < 1)
         {
-            // כאן הוספתי את ה-Transform של אובייקט ה-SceneChanger
-            SoundFXManager.Instance.PlaySoundFXClip(starSound, transform, 1f, true);
-
-            // אפקט ויזואלי לכוכב
-            Instantiate(vfx, stars[0].transform.position, Quaternion.identity);
-
-            stars[0].gameObject.SetActive(true);
-            starsEndScree[0].gameObject.SetActive(true);
-            starCount = 1;
+            GainStar(0);
         }
         if (progress >= 0.8f && starCount < 2)
         {
-            // כאן הוספתי את ה-Transform של אובייקט ה-SceneChanger
-            SoundFXManager.Instance.PlaySoundFXClip(starSound, transform, 1f, true);
-
-            // אפקט ויזואלי לכוכב
-            Instantiate(vfx, stars[1].transform.position, Quaternion.identity);
-
-            stars[1].gameObject.SetActive(true);
-            starsEndScree[1].gameObject.SetActive(true);
-            starCount = 2;
+            GainStar(1);
         }
         if (progress >= 1.0f && starCount < 3)
         {
-            // כאן הוספתי את ה-Transform של אובייקט ה-SceneChanger
-            SoundFXManager.Instance.PlaySoundFXClip(starSound, transform, 1f, true);
-
-            // אפקט ויזואלי לכוכב
-            Instantiate(vfx, stars[2].transform.position, Quaternion.identity);
-
-            stars[2].gameObject.SetActive(true);
-            starsEndScree[2].gameObject.SetActive(true);
-            starCount = 3;
+            GainStar(2);
         }
 
-        // שמור כוכבים
-        SaveManager.Instance.SaveLevelStars(levelNum, starCount); // שמור לשלב 3, 2 כוכבים
+        // שמירת כוכבים
+        SaveManager.Instance.SaveLevelStars(levelNum, starCount);
+    }
+
+    void GainStar(int index)
+    {
+        SoundFXManager.Instance.PlaySoundFXClip(starSound, transform, 1f, true);
+
+        // הכוכב המקורי נצבע בצהוב ונשאר במקום גם כשהזמן עצר
+        stars[index].DOColor(filledStarColor, 0.3f).SetUpdate(true);
+
+        // שכפול זמני של הכוכב לאנימציה
+        CreateStarEffect(stars[index]);
+
+        // עדכון מונה הכוכבים
+        starCount = index + 1;
+    }
+
+
+    void CreateStarEffect(Image originalStar)
+    {
+        // שכפול הכוכב
+        GameObject tempStar = Instantiate(originalStar.gameObject, originalStar.transform.parent);
+        tempStar.transform.SetSiblingIndex(originalStar.transform.GetSiblingIndex() + 1);
+
+        Image tempImage = tempStar.GetComponent<Image>();
+        tempImage.color = filledStarColor;
+
+        tempStar.transform.DOScale(1.08f, 0.25f)
+            .SetEase(Ease.OutBack)
+            .SetUpdate(true) // רץ גם כש-Time.timeScale = 0
+            .OnComplete(() =>
+            {
+                tempStar.transform.DOScale(1f, 0.15f)
+                    .SetEase(Ease.InOutSine)
+                    .SetUpdate(true);
+            });
+
+        tempStar.transform.DOShakePosition(0.3f, new Vector3(4f, 4f, 0f), 10)
+            .SetUpdate(true);
+
+        tempImage.DOFade(0f, 1f)
+            .SetDelay(0.1f)
+            .SetUpdate(true)
+            .OnComplete(() => Destroy(tempStar));
+
+    }
+
+    // אנימציה למסך הסיום כמו באפקט בזמן המשחק
+    void AnimateEndScreenStars(List<Image> starsList, int count)
+    {
+        StartCoroutine(AnimateEndStarsCoroutine(starsList, count));
+    }
+
+    private IEnumerator AnimateEndStarsCoroutine(List<Image> starsList, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            int index = i;
+
+            // הכוכב המקורי הופך לצהוב
+            starsList[index].DOColor(filledStarColor, 0.3f).SetUpdate(true);
+
+            // אפקט שכפול מותאם למסך הסיום
+            CreateEndScreenStarEffect(starsList[index]);
+
+            // סאונד
+            SoundFXManager.Instance.PlaySoundFXClip(starSound, transform, 1f, true);
+
+            // חצי שניה בין כוכב לכוכב
+            yield return new WaitForSecondsRealtime(0.5f);
+        }
+    }
+
+
+    void CreateEndScreenStarEffect(Image originalStar)
+    {
+        GameObject tempStar = Instantiate(originalStar.gameObject, originalStar.transform.parent);
+        tempStar.transform.SetSiblingIndex(originalStar.transform.GetSiblingIndex() + 1);
+
+        Image tempImage = tempStar.GetComponent<Image>();
+        tempImage.color = filledStarColor;
+
+        // נשארים בגודל המקורי ואז מגדילים מזערית
+        Vector3 originalScale = originalStar.transform.localScale;
+        tempStar.transform.localScale = originalScale;
+
+        tempStar.transform.DOScale(originalScale * 1.05f, 0.25f) // הגדלה מזערית ב-5%
+            .SetEase(Ease.OutBack)
+            .SetUpdate(true)
+            .OnComplete(() =>
+            {
+                tempStar.transform.DOScale(originalScale, 0.15f) // חוזר לגודל המקורי
+                    .SetEase(Ease.InOutSine)
+                    .SetUpdate(true);
+            });
+
+        // רטט קטן מאוד
+        tempStar.transform.DOShakePosition(0.3f, new Vector3(0.2f, 0.2f, 0f), 8)
+            .SetUpdate(true);
+
+        // Fade הדרגתי
+        tempImage.DOFade(0f, 1f)
+            .SetDelay(0.1f)
+            .SetUpdate(true)
+            .OnComplete(() => Destroy(tempStar));
     }
 
     public List<Goal> GetGoals()
@@ -258,5 +320,4 @@ public class GameManager : MonoBehaviour
         public CandyType type;
         public int goalPerType;
     }
-
 }
