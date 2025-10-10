075a27d8209e1ebb5ffa2b86c3e51781ea76465c
lavi <lavinaorab@gmail.com>
2025-10-10 13:11:40 +0300

    bygs whid bild

--- Assets/JMO Assets/Welcome Screen/CFXR_WelcomeScreen.cs
+++ Assets/JMO Assets/Welcome Screen/CFXR_WelcomeScreen.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -15,7 +16,7 @@ namespace CartoonFX
                 {
                     return;
                 }
-            SessionState.SetBool("CFXR_WelcomeScreen_Shown", true);
+                SessionState.SetBool("CFXR_WelcomeScreen_Shown", true);
 
                 var importer = AssetImporter.GetAtPath(AssetDatabase.GUIDToAssetPath("bfd03f272fe010b4ba558a3bc456ffeb"));
                 if (importer != null && importer.userData == "dontshow")
@@ -73,3 +74,4 @@ namespace CartoonFX
         }
     }
 }
+#endif
